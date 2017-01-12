7802daa5b2d8b907b949a3a751d526b137337e74
Duy Khoa <duykhoa12t@gmail.com>
2017-01-12 18:38:46 +0800

    add tomdoc for shorten_url & base16 converter

--- app/services/base16_converter.rb
+++ app/services/base16_converter.rb
@@ -1,11 +1,41 @@
+# Public: A encode strategy, convert the id of ShortUrl entity to Base16
+#   It is injected in ShortenUrl service
+#
+# Examples
+#
+#   base16_converter = Base16Converter.new
+#   shorten_url_service = ShortenUrl.new(encode_strategy: base16_converter)
 class Base16Converter
   BASE = 16
 
+  # Public: convert a Numeric to base 16 string
+  #
+  #   value - The Integer to convert to base 16
+  #
+  # Examples
+  #
+  #   base16_converter = Base16Converter.new
+  #   base16_converter.encode(1) #=> '1'
+  #   base16_converter.encode(15) #=> 'f'
+  #   base16_converter.encode(17) #=> '11'
+  #
+  # Returns a String base 16
   def encode(value)
     value.to_s(BASE)
   end
 
-  def decode(value)
-    value.to_i(BASE)
+  # Public: convert a String base16 to integer
+  #
+  #   base16_string - The String in base 16 to convert
+  #
+  # Examples
+  #
+  #   base16_converter = Base16Converter.new
+  #   base16_converter.decode('1') #=> 1
+  #   base16_converter.decode('10') #=> 16
+  #
+  # Returns a Integer
+  def decode(base16_string)
+    base16_string.to_i(BASE)
   end
 end
--- app/services/shorten_url.rb
+++ app/services/shorten_url.rb
@@ -2,10 +2,41 @@ class ShortenUrl
   SHORT_URL_BASE = "http://short.link"
   SHORT_URL_FORMAT = "%<base>s/%<short_id>s"
 
+  # Constructor ShortenUrl service
+  #
+  #   encode_strategy - the object handles responsibility to encode/decode
+  #   the real id of short_url.
+  #
+  # Examples
+  #
+  #   # Use default converter
+  #   ShortenUrl.new
+  #
+  #   # Specific the encode strategy (Base16Converter)
+  #   ShortenUrl.new(encode_strategy: Base16Converter.new
+  #
+  #   More information about the encode_strategy object, checkout
+  #   Base16Converter class
   def initialize(options = {})
     @encode_strategy = options[:encode_strategy] || Base16Converter.new
   end
 
+  # Public: shorten a link
+  #
+  #   url     - The String full url needs to be shorten
+  #   user_id - The Integer user_id, (default: nil)
+  #
+  # Examples
+  #
+  #   shorten_url_service = ShortenUrl.new
+  #
+  #   # Guest wants to shorten a url
+  #   shorten_url_service.shorten("https://google.com") #=> "http://short.link/1"
+  #
+  #   # A user wants to shorten a url
+  #   shorten_url_service.shorten("https://google.com", 1) #=> "http://short.link/1"
+  #
+  # Return a String of shorten url
   def shorten(url, user_id = nil)
     short_url = ShortUrl.find_or_create_by(full_url: url, user_id: user_id)
 
@@ -15,6 +46,16 @@ class ShortenUrl
     }
   end
 
+  # Public: get full url of a short url
+  #
+  #   short_url - The string short url
+  #
+  # Examples
+  #
+  #   shorten_url_service = ShortenUrl.new
+  #   shorten_url_service.full_url("http://short.link/1") #=> "https://google.com"
+  #
+  # Return the String of full url
   def full_url(short_url)
     id = short_url.split("/")[-1]
     return nil unless id
